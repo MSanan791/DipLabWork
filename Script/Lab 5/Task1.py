eef78aac99cfc500185e81fa6351c4f830855653
user.email <sanan791@gmail.com>
2025-02-24 14:34:08 +0500

    late night commit

---
+++ Script/Lab 5/Task1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2 as cv
+
+def readimg_grey(path):
+    img = cv.imread(path, cv.IMREAD_GRAYSCALE)
+    img_data = np.array(img)
+    return img_data
+def readimg_color(path):
+    img = cv.imread(path)
+    img_data = np.array(img)
+    return img_data
+
+img1 = readimg_grey(r"D:\Semester 6\DIP\Lab\Lab 5\lena.png")
+img1_mean = np.mean(img1)
