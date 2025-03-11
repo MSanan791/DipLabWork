a6674e1606a18da38aec9c996a1fd988281795c2
MSanan791 <sanan791@gmail.com>
2025-03-11 19:41:54 +0500

    sdfd

--- Script/Pycodes/Task 3.py
+++ Script/Pycodes/Task 3.py
@@ -1,15 +1,16 @@
-# %%
 import numpy as np
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 def mask_img(image):
     mask = np.ones((3,3,1), dtype=np.uint8)/9
-    x,y = image.shape
+    x, y = image.shape
     padd_img = pad_img(image)
 
-    for i in range(1,x+1):
-        for j in range(1,y+1):
-            padd_img[i,j] = np.sum(mask[0:2, 0:2] * padd_img[i-1:i+1,j-1:j+1])
+    for i in range(1, x+1):
+        for j in range(1, y+1):
+            padd_img[i, j] = np.sum(mask[0:2, 0:2] * padd_img[i-1:i+1, j-1:j+1])
+
     padd_img = unpad_img(padd_img)
     return padd_img
 
@@ -28,10 +29,16 @@ def unpad_img(image):
     unpadded_image = image[1:x_mask+1, 1:y_mask+1]
     return unpadded_image
 
-img = cv.imread(r"fig05.tif", cv.IMREAD_GRAYSCALE)
-img_data = np.array(img)
-cv.imshow("img1", img_data)
-cv.waitKey(0)
-mask = mask_img(img_data)
-cv.imshow("img1", mask)
-cv.waitKey(0)
+img = cv.imread("fig05.tif", cv.IMREAD_GRAYSCALE)
+mask = mask_img(img)
+
+# Use matplotlib to display images
+plt.subplot(1, 2, 1)
+plt.title("Original Image")
+plt.imshow(img, cmap='gray')
+
+plt.subplot(1, 2, 2)
+plt.title("Filtered Image")
+plt.imshow(mask, cmap='gray')
+
+plt.show()
