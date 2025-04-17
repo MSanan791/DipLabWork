36abbcee4e871d08cf8e7c5c7e33e113b73b268e
user.email <sanan791@gmail.com>
2025-04-17 14:37:37 +0500

    late night commit

--- Script/Lab 10(LBP+HOG)/Task1(LBP).py
+++ Script/Lab 10(LBP+HOG)/Task1(LBP).py
@@ -91,7 +91,7 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from picamera2 import Picamera2
+from picamera import Picamera2
 import time
 
 uniform = [0, 1, 2, 3, 4, 6, 7, 8, 12, 14, 15, 16, 24, 28, 30, 31, 32, 48, 56, 60, 62, 63, 64, 96, 112, 120, 124, 126, 127, 128, 129, 131, 135, 143, 159, 191, 192, 193, 195, 199, 207, 223, 224, 225, 227, 231, 239, 240, 241, 243, 247, 248, 249, 251, 252, 253, 254, 255]
@@ -155,6 +155,7 @@ def lbpR(img):
 picam2 = Picamera2()
 picam2.configure(picam2.preview_configuration(main={"format": "RGB888", "size": (320, 240)}))
 picam2.start()
+
 time.sleep(2)
 
 try:
